a02ba267747ffabdb8698eb71b03611183cd6298
faaizonebaariq-cpu <faaizonebaariq@gmail.com>
2026-08-08 09:22:33 +0800

    Added the ping pong for one side

--- ping pong game/main.py
+++ ping pong game/main.py
@@ -9,16 +9,40 @@ window = display.set_mode((win_width, win_height))
 display.set_caption('ping pong game')
 background = transform.scale(image.load(img_back), (win_width, win_height))
 
-finish = False
-run = True
+clock = time.Clock()
+FPS = 60
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - self.rect.height:
+            self.rect.y += self.speed
+
+batman = Player('bat.png', 5, 424, 4)
+
+run = True
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
 
-    window.blit(background, (0, 0))
+    window.blit(background, (0, 0))   
+    batman.update()
+    batman.reset()
 
     display.update()
-    time.delay(50)
-
+    clock.tick(FPS)  
